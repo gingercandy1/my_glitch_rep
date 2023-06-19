d212092ecd20bc199f7bbe722bfa670e0eb35508
Eather Racio <125983428+gingercandy1@users.noreply.github.com>
2023-06-19 21:09:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 #NEZHA_TLS="$NEZHA_TLS"
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-#ARGO_AUTH=''
-#ARGO_DOMAIN="$ARGO_DOMAIN"
+ARGO_AUTH=''
+ARGO_DOMAIN="$ARGO_DOMAIN"
 
 # ttyd / filebrowser argo 域名
 #SSH_DOMAIN="$SSH_AUTH"
@@ -481,10 +481,10 @@ EOF
 }
 
 generate_config
-#generate_argo
+generate_argo
 #generate_nezha
-#generate_ttyd
-#generate_filebrowser
+generate_ttyd
+generate_filebrowser
 
 #[ -e nezha.sh ] && bash nezha.sh
 #[ -e argo.sh ] && bash argo.sh
